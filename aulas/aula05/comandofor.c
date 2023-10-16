8c65ef0385456e08e090a117b649cada8c0227bb
Ariok16 <142408427+Ariok16@users.noreply.github.com>
2023-10-16 13:47:49 +0000

    aula  dia 16/10

---
+++ aulas/aula05/comandofor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+int numero = 0;
+
+  printf("Entre com um numero entre 1 e 10:");
+  int leu_certo = scanf("%i", &numero);
+
+  for(int i = 0; i > -1; i++) {
+printf("Ao infinito e alem!\n");
+  }
+  
+  
+   // printf("%i x 1 = %i\n", numero, numero * 1);
+   // printf("%i x 2 = %i\n", numero, numero * 2);
+   // printf("%i x 3 = %i\n", numero, numero * 3);
+   // printf("%i x 4 = %i\n", numero, numero * 4);
+   // printf("%i x 5 = %i\n", numero, numero * 5);
+   // printf("%i x 6 = %i\n", numero, numero * 6);
+   // printf("%i x 7 = %i\n", numero, numero * 7);
+   // printf("%i x 8 = %i\n", numero, numero * 8);
+   // printf("%i x 9 = %i\n", numero, numero * 9);
+   // printf("%i x 10 = %i\n", numero, numero * 10);
+    
---
+++ praticas/praticas03/questao09.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+int numero = 0;
+
+printf("Entre um numero inteiro: ");
+  scanf("%i", &numero);
+
+  int maior = numero;
+  int menor = numero;
+
+  for(int i=0; i < 9; i++) {
+    printf("Entre outro numero inteiro: ");
+    int leu_certo = scanf("%i", &numero);
+
+    if (numero > maior) {
+      maior = numero;
+    }
+
+    if (numero < menor) {
+      menor = numero;
+    }
+  }
+
+  printf("O maior numero eh %i e o menor eh %i\n", maior, menor);
+
+  
+  return 0;
+}
